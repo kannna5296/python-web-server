21ea91c06aba70cc3a13a94e1abc8dd4be68729f
kannna5296 <kannnahacks5296@gmail.com>
2025-06-20 15:49:31 +0900

    format

--- tcpserver.py
+++ tcpserver.py
@@ -2,51 +2,56 @@ from operator import add
 import socket
 from typing import Required
 
+
 # ソケット通信(=トランスポート層)
-# TCP/IPで言うと　
+# TCP/IPで言うと
 #   TCP/UDPを使うところ
 #   インターネット層(IP)の一個上
 #   アプリケーション層(HTTP,SNTPとか)の一個下
 class TCPServer:
-  def serve(self):
+    def serve(self):
 
-    log("サーバ起動します")
-    try:
-      server_socket = socket.socket()
+        log("サーバ起動します")
+        try:
+            server_socket = socket.socket()
 
-      # 1引数...どのレイヤのものか？ SOL_SOCKETだとソケット自体に関するオプション
-      # 2個目...付け足したいオプション
-      # 3個目...true or false
-      # socket.SO_REUSEADDR: 待ち状態中のポートが存在してもbindする
-      server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            # 1引数...どのレイヤのものか？ SOL_SOCKETだとソケット自体に関するオプション
+            # 2個目...付け足したいオプション
+            # 3個目...true or false
+            # socket.SO_REUSEADDR: 待ち状態中のポートが存在してもbindする
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-      server_socket.bind(("localhost", 8080))
-      server_socket.listen(10) #10は同時接続数みたいなもん
+            server_socket.bind(("localhost", 8080))
+            server_socket.listen(10)  # 10は同時接続数みたいなもん
 
-      log("クライアントからの接続を待ちます")
-      # 接続完了したsocketインスタンスと、クライアントのaddressがもらえる
-      (client_socket, address) = server_socket.accept()
-      log("クライアントからの接続が完了しました remote_address: {}", address)
+            log("クライアントからの接続を待ちます")
+            # 接続完了したsocketインスタンスと、クライアントのaddressがもらえる
+            (client_socket, address) = server_socket.accept()
+            log("クライアントからの接続が完了しました remote_address: {}", address)
 
-      request = client_socket.recv(4096) #データを4096バイトずつ受け取る
+            request = client_socket.recv(4096)  # データを4096バイトずつ受け取る
 
-      # ログ追記
-      with open("server_recv.txt", "wb") as f:
-        f.write(request)
+            # ログ追記
+            with open("server_recv.txt", "wb") as f:
+                f.write(request)
 
-      # 返事は特にせずに通信終了
-      client_socket.close()
-    finally:
-      log("サーバ停止しました")
+            # 返事は特にせずに通信終了
+            client_socket.close()
+        finally:
+            log("サーバ停止しました")
+
 
 """
   見やすくログを出すメソッド。プレースホルダ対応。
 """
+
+
 def log(msg, *args):
-  PRE_SUFFIX = "=="
-  formatted = msg.format(*args)
-  print(f"{PRE_SUFFIX} {formatted} {PRE_SUFFIX}")
+    PRE_SUFFIX = "=="
+    formatted = msg.format(*args)
+    print(f"{PRE_SUFFIX} {formatted} {PRE_SUFFIX}")
+
 
 if __name__ == "__main__":
-  server = TCPServer()
-  server.serve()
+    server = TCPServer()
+    server.serve()
