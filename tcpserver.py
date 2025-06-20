7b761c57a63055259859fa0ee19097a1761d38bb
kannna5296 <kannnahacks5296@gmail.com>
2025-06-20 15:58:57 +0900

    docstringの位置修正

--- tcpserver.py
+++ tcpserver.py
@@ -40,13 +40,10 @@ class TCPServer:
         finally:
             log("サーバ停止しました")
 
-
-"""
-  見やすくログを出すメソッド。プレースホルダ対応。
-"""
-
-
 def log(msg, *args):
+    """
+    見やすくログを出すメソッド。プレースホルダ対応。
+    """
     PRE_SUFFIX = "=="
     formatted = msg.format(*args)
     print(f"{PRE_SUFFIX} {formatted} {PRE_SUFFIX}")
