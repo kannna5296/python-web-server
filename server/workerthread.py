6c87d02044e7d1bc3f30e31e85f74c6c53f8d70f
kannna5296 <kannnahacks5296@gmail.com>
2025-06-22 16:48:57 +0900

    ちょいリファクタ

--- server/workerthread.py
+++ server/workerthread.py
@@ -1,11 +1,14 @@
 import os
+import re
 import traceback
 from datetime import datetime, timezone
 from socket import socket
 from typing import Tuple
 from utils import log
+from threading import Thread
 
-class WorkerThread:
+
+class WorkerThread(Thread):
     """
     WEBサーバが期待される挙動をするスレッドを定義
     """
@@ -38,55 +41,24 @@ class WorkerThread:
             with open("server_recv.txt", "wb") as f:
                 f.write(request)
 
-            # リクエスト全体を
-            # 1. リクエストライン(1行目)
-            # 2. リクエストヘッダー(2行目〜空行)
-            # 3. リクエストボディ(空行〜)にパースする
-            request_line, remain = request.split(b"\r\n", maxsplit=1)
-            request_header, request_body = remain.split(b"\r\n\r\n", maxsplit=1)
+            # リクエストパース
+            method, path, http_version, request_header, request_body = (
+                self.parse_request(request)
+            )
 
-            method, path, http_version = request_line.decode().split(" ")
+            # 静的ファイルのパスを取得
+            static_file_path = self.get_static_file(path)
 
-            log("path: " + path)
-            relative_path = path.lstrip("/")
-            log("relative_path: " + relative_path)
+            # レスポンスライン、ボディ生成
+            response_line, response_body = self.create_response_body(static_file_path)
 
-            # デフォルトパス指定
-            if not relative_path:
-                relative_path = "index.html"
-            static_file_path = os.path.join(self.STATIC_ROOT, relative_path)
-            log("static_file_path: " + static_file_path)
-
-            try:
-                # ファイルからレスポンスボディを生成　バイナリ、読み取り専用モード
-                with open(static_file_path, "rb") as f:
-                    response_body = f.read()
-                # レスポンスラインを生成
-                response_line = "HTTP/1.1 200 OK\r\n"
-            except OSError:
-                log("ファイルが見つかりませんでした")
-                # ファイルが見つからなかった場合は404を返す
-                response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
-                response_line = "HTTP/1.1 404 Not Found\r\n"
-
-            if "." in path:
-                ext = path.rsplit(".", maxsplit=1)[-1]
-            else:
-                ext = ""
-            content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
-
-            # レスポンスヘッダーを生成
-            response_header = ""
-            response_header += f"Date: {datetime.now(timezone.utc).strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
-            response_header += "Host: HenaServer/0.1\r\n"
-            response_header += f"Content-Length: {len(response_body)}\r\n"
-            response_header += "Connection: Close\r\n"
-            response_header += f"Content-Type: {content_type}\r\n"
+            # レスポンスヘッダ生成
+            response_header = self.create_response_header(path, len(response_body))
 
             # ヘッダーとボディを空行でくっつけた上でbytesに変換し、レスポンス全体を生成する
             response = (
                 response_line + response_header + "\r\n"
-            ).encode() + response_body
+            ).encode() + response_body.encode()
 
             # ソケット通信はバイト単位でデータを送受信する必要がある
             # クライアントへレスポンスを送信する
@@ -101,3 +73,55 @@ class WorkerThread:
         finally:
             # 例外が発生した場合も、発生しなかった場合も、TCP通信のcloseは行う
             self.client_socket.close()
+
+    def parse_request(self, request: bytes) -> Tuple[str, str, str, bytes, bytes]:
+        request_line, remain = request.split(b"\r\n", maxsplit=1)
+        request_header, request_body = remain.split(b"\r\n\r\n", maxsplit=1)
+        method, path, http_version = request_line.decode().split(" ")
+
+        return method, path, http_version, request_header, request_body
+
+    def get_static_file(self, path) -> str:
+        log("path: " + path)
+        relative_path = path.lstrip("/")
+        log("relative_path: " + relative_path)
+
+        # デフォルトパス指定
+        if not relative_path:
+            relative_path = "index.html"
+        static_file_path = os.path.join(self.STATIC_ROOT, relative_path)
+        log("static_file_path: " + static_file_path)
+
+        return static_file_path
+
+    def create_response_body(self, static_file_path) -> Tuple[str, str]:
+        try:
+            # ファイルからレスポンスボディを生成　バイナリ、読み取り専用モード
+            with open(static_file_path, "rb") as f:
+                response_body = f.read()
+            # レスポンスラインを生成
+            response_line = "HTTP/1.1 200 OK\r\n"
+        except OSError:
+            log("ファイルが見つかりませんでした")
+            # ファイルが見つからなかった場合は404を返す
+            response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
+            response_line = "HTTP/1.1 404 Not Found\r\n"
+
+        return response_line, response_body.decode()
+
+    def create_response_header(self, path, content_length) -> str:
+        if "." in path:
+            ext = path.rsplit(".", maxsplit=1)[-1]
+        else:
+            ext = ""
+        content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
+
+        # レスポンスヘッダーを生成
+        response_header = ""
+        response_header += f"Date: {datetime.now(timezone.utc).strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
+        response_header += "Host: HenaServer/0.1\r\n"
+        response_header += f"Content-Length: {content_length}\r\n"
+        response_header += "Connection: Close\r\n"
+        response_header += f"Content-Type: {content_type}\r\n"
+
+        return response_header
