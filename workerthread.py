3cf1fa4462e5205c9af9a5eaa6680dd16355cd5a
kannna5296 <kannnahacks5296@gmail.com>
2025-06-23 12:02:11 +0900

    urlsを切り出し

--- workerthread.py
+++ workerthread.py
@@ -9,8 +9,8 @@ from henango.http.request import HttpRequest
 from henango.http.response import HttpResponse
 from mylog import log
 from threading import Thread
-from pprint import pformat
-import views
+from urls import URL_VIEW
+
 
 
 class WorkerThread(Thread):
@@ -30,13 +30,6 @@ class WorkerThread(Thread):
         "png": "image/png",
         "jpg": "image/jpg",
         "gif": "image/gif",
-    }
-
-    # pathとview関数の対応
-    URL_VIEW = {
-        "/now": views.now,
-        "/show_request": views.show_request,
-        "/parameters": views.parameters,
     }
 
     # ステータスコードとステータスラインの対応
@@ -82,8 +75,8 @@ class WorkerThread(Thread):
             content_type: Optional[str]
             response_line: str
             # pathに対応するview関数があれば、関数を取得して呼び出し、レスポンスを生成する
-            if request.path in self.URL_VIEW:
-                view = self.URL_VIEW[request.path]
+            if request.path in URL_VIEW:
+                view = URL_VIEW[request.path]
                 response = view(request)
             # pathがそれ以外のときは、静的ファイルからレスポンスを生成す
             else:
