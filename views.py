3f9c9a10507d7686ce545b0c561d1e80a170bb30
kannna5296 <kannnahacks5296@gmail.com>
2025-06-23 13:25:02 +0900

    全URLについてテンプレート化

--- views.py
+++ views.py
@@ -13,71 +13,33 @@ def now(request: HttpRequest) -> HttpResponse:
     body = render("now.html", context)
     return HttpResponse(body=body)
 
-
 def show_request(request: HttpRequest) -> HttpResponse:
-    html = f"""\
-        <html>
-        <body>
-            <h1>Request Line:</h1>
-            <p>
-                {request.method} {request.path} {request.http_version}
-            </p>
-            <h1>Headers:</h1>
-            <pre>{pformat(request.headers)}</pre>
-            <h1>Body:</h1>
-            <pre>{request.body.decode("utf-8", "ignore")}</pre>
-            
-        </body>
-        </html>
-    """
-    body = textwrap.dedent(html).encode()
-
-    # Content-Typeを指定
-    content_type = "text/html, charset=UTF-8"
-
-    return HttpResponse(body=body, content_type=content_type, status_code=200)
+    context = {
+        "now": datetime.now(),
+        "method": request.method,
+        "path": request.path,
+        "http_version": request.http_version,
+        "headers": pformat(request.headers),
+        "body": request.body.decode("utf-8", "ignore")
+        }
+    body = render("show_request.html", context)
+    return HttpResponse(body=body)
 
 
 def parameters(request: HttpRequest) -> HttpResponse:
     """
     POSTパラメータを表示するHTMLを表示する
     """
-
-    # GETリクエストの場合は、405を返す
-    if request.method == "GET":
+    if request.method == "POST":
+        context = {"parameters": pformat(parse_qs(request.body.decode()))}
+        body = render("parameters.html", context)
+        return HttpResponse(body=body)
+    else:
         body = b"<html><body><h1>405 Method Not Allowed</h1></body></html>"
-        content_type = "text/html; charset=UTF-8"
-        status_code = 405
-
-    elif request.method == "POST":
-        post_params = parse_qs(request.body.decode())
-        html = f"""\
-            <html>
-            <body>
-                <h1>Parameters:</h1>
-                <pre>{pformat(post_params)}</pre>                        
-            </body>
-            </html>
-        """
-        body = textwrap.dedent(html).encode()
-        content_type = "text/html; charset=UTF-8"
-        status_code = 200
-
-    return HttpResponse(body=body, content_type=content_type, status_code=status_code)
+        return HttpResponse(body=body, status_code=405)
 
 
 def user_profile(request: HttpRequest) -> HttpResponse:
-    user_id = request.params["user_id"]
-    html = f"""\
-        <html>
-        <body>
-            <h1>プロフィール</h1>
-            <p>ID: {user_id}
-        </body>
-        </html>
-    """
-    body = textwrap.dedent(html).encode()
-    content_type = "text/html; charset=UTF-8"
-    status_code = 200
-
-    return HttpResponse(body=body, content_type=content_type, status_code=status_code)
+    context = {"user_id": request.params["user_id"]}
+    body = render("user_profile.html", context)
+    return HttpResponse(body=body)
